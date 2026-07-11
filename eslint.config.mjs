305987d5481b5aa6954ddfc0d071794fecc5fd0c
Dave <6867885+pixl-scribe@users.noreply.github.com>
2026-07-11 16:20:42 -0600

    Added main method along with eslint.

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error', // Report prettier issues as errors
+    },
+  },
+  prettierConfig // Must be last to override other configs
+);
