caedb7b5c0cfe7fcc037952807d9c3c701c35daf
Dave <6867885+pixl-scribe@users.noreply.github.com>
2026-07-16 16:31:43 -0600

    Finished checksum generation.

--- src/checksum-factory.ts
+++ src/checksum-factory.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { parse } from 'yaml';
+import { parse, stringify } from 'yaml';
 import UsxParser from './usx-parser';
 import path from 'path';
 import { ModuleConfig } from './model/module-config';
@@ -8,7 +8,12 @@ import {
   TestamentVerseCounts,
 } from './model/bible-verse-counts';
 import { Paragraph } from './model/book';
-import { BookChecksum, ChapterChecksum } from './model/checksum-config';
+import {
+  BookChecksum,
+  ChapterChecksum,
+  ModuleChecksum,
+  TestamentChecksum,
+} from './model/checksum-config';
 import { xxHash32 } from 'js-xxhash';
 
 export default class ChecksumFactory {
@@ -31,6 +36,7 @@ export default class ChecksumFactory {
       'utf-8'
     );
     const verseCounts = parse(verseCountsContent) as BibleVerseCounts;
+    const testamentChecksums: TestamentChecksum[] = [];
 
     for (const testament of config.testamentsIncluded) {
       let chapters: TestamentVerseCounts | undefined = undefined;
@@ -43,26 +49,52 @@ export default class ChecksumFactory {
           break;
       }
       const usxFiles = UsxParser.getUsxFiles(moduleId, chapters);
-      ChecksumFactory.checksumTestament(usxFiles, chapters);
+      if (testament === 'OT' || testament === 'NT') {
+        const bookChecksums = ChecksumFactory.checksumTestament(
+          usxFiles,
+          chapters
+        );
+        const bookJoinedChecksums = bookChecksums
+          .map((bcs) => bcs.checksum)
+          .join();
+        testamentChecksums.push({
+          testament,
+          checksum: xxHash32(bookJoinedChecksums),
+          bookChecksums,
+        });
+      }
+    }
+
+    if (testamentChecksums.length > 0) {
+      const testamentJoinedChecksums = testamentChecksums
+        .map((tcs) => tcs.checksum)
+        .join();
+      const moduleChecksum: ModuleChecksum = {
+        checksum: xxHash32(testamentJoinedChecksums),
+        testamentChecksums,
+      };
+      const checksumYaml = stringify(moduleChecksum);
+      ChecksumFactory.writeChecksums(moduleId, checksumYaml);
     }
   }
 
   private static checksumTestament(
     usxFiles: string[],
     chapters: TestamentVerseCounts | undefined
-  ): void {
+  ): BookChecksum[] {
     const usxParser = new UsxParser();
     const bookChecksums: BookChecksum[] = [];
     for (const usxFile of usxFiles) {
       console.log(`  parsing ${usxFile}...`);
       const book = usxParser.parseBook(usxFile);
       const rawBookVerses = usxParser.getRawVerseText();
-      // TODO: stopped here. Need to construct checksum yaml file containing hashes.
-      console.log({ rawBookVerses });
       const chapterChecksums = ChecksumFactory.getVerseChecksums(rawBookVerses);
+      const chapterJoinedChecksums = chapterChecksums
+        .map((ccs) => ccs.checksum)
+        .join();
       bookChecksums.push({
         bookCode: book.code,
-        checksum: 0, // TODO
+        checksum: xxHash32(chapterJoinedChecksums),
         chapterChecksums,
       });
 
@@ -95,9 +127,9 @@ export default class ChecksumFactory {
         }
       });
       console.log(`  ${book.name} verse counts were correct.`);
-      // TODO: remove this
-      break;
     }
+
+    return bookChecksums;
   }
 
   private static getVerseChecksums(
@@ -107,7 +139,7 @@ export default class ChecksumFactory {
       rawBookVerses,
       (verse) => verse.sid.split(':')[0]
     );
-    const chapterChecksums = Object.keys(chapters).map((sid: string) => {
+    return Object.keys(chapters).map((sid: string) => {
       const rawVerses = chapters[sid] as { sid: string; text: string }[];
       const verseChecksums = rawVerses.map(
         ({ sid, text }: { sid: string; text: string }) => ({
@@ -115,15 +147,24 @@ export default class ChecksumFactory {
           checksum: xxHash32(`${sid}|${text}`),
         })
       );
-      console.log({ verseChecksums });
+      const verseJoinedChecksums = verseChecksums
+        .map((vcs) => vcs.checksum)
+        .join();
       return {
         sid,
-        checksum: xxHash32(verseChecksums.join()),
+        checksum: xxHash32(verseJoinedChecksums),
         verseChecksums,
       };
     });
-    console.log({ chapterChecksums });
+  }
 
-    return chapterChecksums;
+  private static writeChecksums(moduleId: string, yaml: string) {
+    const checksumFile = path.resolve('assets', moduleId, 'checksum.yaml');
+    try {
+      fs.writeFileSync(checksumFile, yaml, 'utf8');
+      console.log('  Checksum file written successfully!');
+    } catch (error) {
+      console.error('Error writing checksum file:', error);
+    }
   }
 }
